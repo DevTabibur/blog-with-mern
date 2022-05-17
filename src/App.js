655844fc9c535e9c83363f86d5d06707abcc05dd
DevTabibur <tobiburrohman2@gmail.com>
2022-05-17 11:25:54 +0600

    tailwind is working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Pages/Home/Home';
+import Header from './shared/Header/Header';
+
+// demo site::  https://atixscripts.info/demo/html/minimag/index-13.htm
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline text-red-900">
+    Hello world!
+  </h1>
     </div>
+    </>
   );
 }
 
